a62b933d2e3d23af799bceb46b44d38791269a81
Jeffrey.S.Whitaker <Jeffrey.S.Whitaker@noaa.gov>
2021-07-25 16:42:08 -0400

    remove symbolic links

--- hpss.sh
+++ hpss.sh
@@ -5,23 +5,27 @@ source $MODULESHOME/init/sh
 if [ $machine == "gaea" ]; then
    #module load hsi
    htar=/sw/rdtn/hpss/default/bin/htar
+   hsi=/sw/rdtn/hpss/default/bin/hsi
 else
    module load hpss
    htar=`which htar`
+   hsi=`which hsi`
 fi
 #env
 hsi ls -l $hsidir
 hsi mkdir ${hsidir}/
-cd ${datapath2}
-cd ..
+cd ${datapath2}/control
+find -type l -delete # delete symlinks
+cd ../..
+$hsi mkdir ${hsidir}
 $htar -cvf ${hsidir}/${analdate}.tar ${analdate}
-hsi ls -l ${hsidir}/${analdate}.tar
-if [ $? -eq 0 ]; then
-   cd ${analdate}
-   /bin/rm -f *fg*fhr00* *fg*fhr03* *fg*fhr09* *fg*fhr06*
-   cd control/INPUT
-   /bin/rm -f *tile*nc
-fi
+$hsi ls -l ${hsidir}/${analdate}.tar
+#if [ $? -eq 0 ]; then
+#   cd ${analdate}
+#   /bin/rm -f *fg*fhr00* *fg*fhr03* *fg*fhr09* *fg*fhr06*
+#   cd control/INPUT
+#   /bin/rm -f *tile*nc
+#fi
 cd ${datapath2}
-/bin/rm -f diag*cris* diag*airs* diag*iasi* 
+#/bin/rm -f diag*cris* diag*airs* diag*iasi* 
 exit $exitstat
