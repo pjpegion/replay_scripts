b249724c8f5dfbc4ec65e31a644f5001be6d0e05
Jeffrey S Whitaker <jeffrey.s.whitaker@noaa.gov>
2022-09-01 17:28:51 -0500

    add fv_srf_wnd

--- remove_checksum.py
+++ remove_checksum.py
@@ -3,7 +3,7 @@ from netCDF4 import Dataset
 import sys, os
 datapath = sys.argv[1]
 for tile in ['tile1','tile2','tile3','tile4','tile5','tile6']:
-    for ftype in ['fv_core','fv_tracer','sfc_data','phy_data']: 
+    for ftype in ['fv_core','fv_tracer','fv_srf_wnd','sfc_data','phy_data']: 
         if ftype in ['sfc_data','phy_data']:
             nc = Dataset(os.path.join(datapath,'%s.%s.nc' % (ftype,tile)),'a')
         else:
