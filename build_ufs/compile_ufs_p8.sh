3d8d08dd0fbedc801b4848f93386d5bc4906c8a6
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2022-12-09 18:07:55 +0000

    incorporate Phil's suggestion

--- build_ufs/compile_ufs_p8.sh
+++ build_ufs/compile_ufs_p8.sh
@@ -10,10 +10,19 @@ git submodule update --init --recursive
 
 # get version of dycore that supports mixed mode compilation
 cd FV3
-mv atmos_cubed_sphere atmos_cubed_sphere.save
-git clone https://github.com/NOAA-EMC/GFDL_atmos_cubed_sphere atmos_cubed_sphere
+
+#mv atmos_cubed_sphere atmos_cubed_sphere.save
+#git clone https://github.com/NOAA-EMC/GFDL_atmos_cubed_sphere atmos_cubed_sphere
+#cd atmos_cubed_sphere
+#git checkout fms_mixedmode
+#cd ../..
+
 cd atmos_cubed_sphere
-git checkout fms_mixedmode
+git checkout master
+git pull
+git remote add upstream https://github.com/NOAA-EMC/GFDL_atmos_cubed_sphere
+git fetch upstream
+git checkout upstream/fms_mixedmode
 cd ../..
 
 # patch to turn off saving of IAU increments in MOM6 restart
@@ -24,9 +33,8 @@ patch -p 0 < ../mixedmode_p8.diff
 patch -p 0 < ../mom_cap.diff
 
 cd tests
-./compile.sh hera.intel "-DAPP=S2SW -D32BIT=ON -DCCPP_SUITES=FV3_GFS_v17_coupled_p8" coupled YES NO
-cd ../
-./compile.sh hera.intel "-DAPP=ATM -D32BIT=ON -DCCPP_SUITES=FV3_GFS_v17_p8" atmonly YES NO
+./compile.sh ${machine}.intel "-DAPP=S2SW -D32BIT=ON -DCCPP_SUITES=FV3_GFS_v17_coupled_p8" coupled YES NO
+./compile.sh ${machine}.intel "-DAPP=ATM -D32BIT=ON -DCCPP_SUITES=FV3_GFS_v17_p8" atmonly YES NO
 /bin/cp -f ufs-weather-model/tests/fv3_coupled.exe ../exec_${machine}/ufs_coupled.exe
 /bin/cp -f ufs-weather-model/tests/fv3_atmonly.exe ../exec_${machine}/fv3_atm.exe
 /bin/rm -rf ufs-weather-model
