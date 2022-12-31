ade971f7211f37e646dcc03a73635d9539abd87b
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2022-12-31 23:33:45 +0000

    use nccompress

--- hpss.sh
+++ hpss.sh
@@ -9,18 +9,11 @@ if [ $machine == "gaea" ]; then
    module load netcdf
 else
    module load hpss
-   module use /scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack
-   module load hpc/1.2.0
-   module load hpc-intel/2022.1.2
-   module load hpc-impi/2022.1.2
-   module load netcdf/4.7.4
-   module load nco/4.9.1
    htar=`which htar`
    hsi=`which hsi`
-   nccopy=`which nccopy`
-   ncdump=`which ncdump`
-   ncks=`which ncks`
 fi
+nccompress=/home/Jeffrey.S.Whitaker/.local/bin/nccompress
+
 #env
 #$hsi ls -l $hsidir
 $hsi mkdir -p ${hsidir}/
@@ -34,31 +27,23 @@ HH=`echo $analdate | cut -c9-10`
 /bin/mv -f ${datapath2}/control/INPUT/fv3_increment6.nc ${datapath2}
 
 cd ${datapath2}/control
-find -type l -delete # delete symlinks
-/bin/rm -f core
+find -type l -delete # delete symlinks and core files
 
 $hsi mkdir -p ${hsidir}
 
 # archive restarts only at 06Z (for 00Z 'analysis' time)
 if [ $HH == '06' ]; then
-   # re-write restarts compressed
    cd ${datapath2}/control/INPUT
-   for file in *nc; do
-       compressed=`$ncdump -sh $file | grep Deflate`
-       if [ ! -z "$compressed" ]; then
-          echo "$file already compressed"
-       else
-          #$nccopy -4 -d 1 -s $file ${file}.compressed
-          $ncks -O -4 -L 1 $file ${file}.compressed
-          if [ $? -eq 0 ]; then
-             /bin/mv -f ${file}.compressed $file
-          else
-             echo "$file not compressed"
-             /bin/rm -f ${file}.compressed*
-          fi
-       fi
-   done
+   # compress restart files.
+   if [ -f $nccompress ]; then
+      find . -iname "*.nc" > list.txt
+      $nccompress -d 1 -pa -o --ff list.txt
+      rm list.txt
+   else
+      echo "nccompress not found, not compressing restarts"
+   fi
    cd ${datapath}
+   /bin/rm -f ${analdate}/control/INPUT/core.* ${analdate}/control/core.*
    /bin/rm -f ${analdate}/control/*.out_grd.ww3 ${analdate}/control/*.restart.ww3
    $htar -cvf ${hsidir}/${analdate}.restart.tar ${analdate}/control ${analdate}/GFSPRS.GrbF03 ${analdate}/GFSFLX.GrbF03 
    exitstat=$?
@@ -69,29 +54,20 @@ if [ $HH == '06' ]; then
 fi
 cd $datapath
 /bin/rm -f ${analdate}/control/*.out_grd.ww3 ${analdate}/control/*.restart.ww3
-/bin/mv -f ${analdate}/control control.save
+/bin/mv -f ${analdate}/control control.save # move directory out of the way
 /bin/rm -rf ${analdate}/GFS*06 ${analdate}/GFS*09 # remove restarts, keep fh=3 grib files
-# compress ocean history file(s)
 cd $datapath2
-for file in ocn_*nc; do
-   compressed=`$ncdump -sh $file | grep Deflate`
-   if [ ! -z "$compressed" ]; then
-      echo "$file already compressed"
-   else
-      #$nccopy -4 -d 1 -s $file ${file}.compressed
-      $ncks -O -4 -L 1 $file ${file}.compressed
-      if [ $? -eq 0 ]; then
-         /bin/mv -f ${file}.compressed $file
-      else
-         echo "$file not compressed"
-         /bin/rm -f ${file}.compressed*
-      fi
-   fi
-done
+# compress ocean history files
+if [ -f $nccompress ]; then
+   $nccompress -d 1 -o ocn_*nc
+else
+   echo "nccompress not found, not compressing ocn_*nc"
+fi
 cd $datapath
+/bin/rm -f ${analdate}/core.*
 htar -cvf ${hsidir}/${analdate}.history.tar ${analdate}
 exitstat=$?
-/bin/mv -f control.save ${analdate}/control
+/bin/mv -f control.save ${analdate}/control # move directory back
 if [ $exitstat -ne 0 ]; then
    echo "creating history tar file failed"
    exit $exitstat
